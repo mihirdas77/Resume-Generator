33cb43e366272b821b9eee33627c01e7e7c236b5
mihirdas77 <mihirdas7707@gmail.com>
2024-09-21 13:16:44 +0530

    added

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -5,13 +5,14 @@ const Navbar = () => {
     return (
         <header className="navbar">
             <div className="navbar-container">
-                <h3 className="navbar-title">Resume.io</h3>
+                
                 <nav className="navbar-nav">
-                    <a href="#" className="navbar-link">Home</a>
+                    {/* <a href="#" className="navbar-link">Home</a>
                     <a href="#" className="navbar-link">Templates</a>
-                    <a href="#" className="navbar-link">About</a>
+                    <a href="#" className="navbar-link">About</a> */}
                     <a href='mailto:mihirdas7707@gmail.com' aria-label="Email"><button  className="contact-button">Contact</button></a>
-                </nav>                
+                </nav>  
+                <h3 className="navbar-title">Resume.io</h3>              
             </div>
         </header>
     );
